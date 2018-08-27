b23095c5b4b1389a7c9c700a858b989bcb6b20b5
dantebarnes <dcbarnes@aggies.ncat.edu>
2018-08-27 16:11:47 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { FileUploader } from "carbon-components";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
+// let fileUploaderInstance =
+
+FileUploader.create(document.getElementById("my-file"));
+// fileUploaderInstance.setState("complete", 1);
